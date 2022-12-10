9c0c2aa60696bef65358ba0b79042d76a5a783bc
Will Angelis <willangelis@gmail.com>
2022-12-10 01:10:58 -0300

    add font roboto and change global css

--- repo-github-dio/src/styles/global.ts
+++ repo-github-dio/src/styles/global.ts
@@ -1,14 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
   *, body {
+    border: 0;
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;    
   }
   body {
     background-color: #22272e;
     color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
   }
-
 `;
