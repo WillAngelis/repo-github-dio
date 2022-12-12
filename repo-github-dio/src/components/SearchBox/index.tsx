86304620d0cf99be597515a0b7f4ca8e2ea43884
Will Angelis <willangelis@gmail.com>
2022-12-11 23:15:37 -0300

    change name of variable inside search box to something more clean

--- repo-github-dio/src/components/SearchBox/index.tsx
+++ repo-github-dio/src/components/SearchBox/index.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 import { InputSearch } from "../Input"
+import { RepoItem } from "../RepoItem";
 import { Container } from "./styles"
 
 export const SearchBox = () => {
+  const [isSearching, setIsSearching] = useState(false);
+
   return (
-    <Container hasResponse={true}>
-      <InputSearch />
+    <Container isSearching={isSearching}>
+      <InputSearch setIsSearching={setIsSearching} isSearching={isSearching} />
+      {/* <RepoItem /> */}
     </Container>
   )
 }
--- repo-github-dio/src/components/SearchBox/styles.ts
+++ repo-github-dio/src/components/SearchBox/styles.ts
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 interface ContainerProps {
-  hasResponse: boolean;
+  isSearching: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
   background-color: #fff;
   color: #000;
-  border-radius: 6px;
-  height: ${(props) => (props.hasResponse ? "400px" : "200px")};
+  border-radius: 18px;
+  height: ${(props) => (props.isSearching ? "400px" : "80px")};
   width: 600px;
-  padding: 8px;
-  transition: all 800ms;
+  padding: 12px 24px;
+  transition: all 900ms;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
 `;
